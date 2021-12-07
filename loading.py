13f07f1e3722711304ac57ab79929dc5431ee6a4
daianaboia <82261607+daianaboia@users.noreply.github.com>
2021-12-07 17:48:47 +0100

    Update loading.py

--- loading.py
+++ loading.py
@@ -9,6 +9,6 @@ drive = GoogleDrive(gauth)
 upload_file_list =  [f for f in listdir('path') if isfile(join('path', f))]
 print(upload_file_list)
 for upload_file in upload_file_list:
-	gfile = drive.CreateFile({'title': upload_file,'parents': [{'id': 'googledrivefolderid'}]})	# Read file and set it as the content of this instance.
+	gfile = drive.CreateFile({'title': upload_file,'parents': [{'id': 'googledrivefolderid'}]})	
 	gfile.SetContentFile("path"+upload_file)
 	gfile.Upload() # Upload the file.
