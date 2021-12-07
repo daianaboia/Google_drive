9c0ab3519d8d878dfc525423029793a87f5a9097
daianaboia <82261607+daianaboia@users.noreply.github.com>
2021-12-07 17:48:23 +0100

    Update loading.py

--- loading.py
+++ loading.py
@@ -6,9 +6,9 @@ from os.path import isfile, join
 gauth = GoogleAuth()           
 drive = GoogleDrive(gauth)  
 
-upload_file_list =  [f for f in listdir('/Users/daianaboia/Programmazione/ProgettoTommaso/Insider_bozza/script/ch/') if isfile(join('/Users/daianaboia/Programmazione/ProgettoTommaso/Insider_bozza/script/ch/', f))]
+upload_file_list =  [f for f in listdir('path') if isfile(join('path', f))]
 print(upload_file_list)
 for upload_file in upload_file_list:
-	gfile = drive.CreateFile({'title': upload_file,'parents': [{'id': '19FUBoFa5Y7KxdGlvrWMUYe_w8aPqFBNc'}]})	# Read file and set it as the content of this instance.
-	gfile.SetContentFile("/Users/daianaboia/Programmazione/ProgettoTommaso/Insider_bozza/script/ch/"+upload_file)
+	gfile = drive.CreateFile({'title': upload_file,'parents': [{'id': 'googledrivefolderid'}]})	# Read file and set it as the content of this instance.
+	gfile.SetContentFile("path"+upload_file)
 	gfile.Upload() # Upload the file.
